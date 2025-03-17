377e3d6d2a9049bd3c5fcec4272be8355d75228a
thilankadw <thilankawijesingham@gmail.com>
2025-03-17 19:14:36 +0530

    initial project

---
+++ codebasicsapi/src/main/java/com/codebasics/codebasics/CodebasicsApplication.java
@@ -0,0 +1,13 @@
+package com.codebasics.codebasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodebasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodebasicsApplication.class, args);
+	}
+
+}
---
+++ codebasicsapi/src/test/java/com/codebasics/codebasics/CodebasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebasics.codebasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodebasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
