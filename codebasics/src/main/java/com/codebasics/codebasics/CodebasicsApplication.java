759a2ac79013f2a4297f72a36e57ed8a3521e38a
Rivin Jayawardana <127378095+IT22061652@users.noreply.github.com>
2025-03-18 19:21:08 +0530

    env

--- codebasics/src/main/java/com/codebasics/codebasics/CodebasicsApplication.java
+++ codebasics/src/main/java/com/codebasics/codebasics/CodebasicsApplication.java
@@ -2,11 +2,21 @@ package com.codebasics.codebasics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
+
 public class CodebasicsApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+		// Set environment variables for Spring Boot
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+		System.setProperty("DATABASE_USER", dotenv.get("DATABASE_USER"));
+		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+
+
 		SpringApplication.run(CodebasicsApplication.class, args);
 	}
 
