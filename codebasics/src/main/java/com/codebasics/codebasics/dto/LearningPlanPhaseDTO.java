0fba49a2795f873ea28e631c92a5436a243d08a0
thilankadw <thilankawijesingham@gmail.com>
2025-04-22 17:47:22 +0530

    perf: updatte learnning plans backend

--- codebasics/src/main/java/com/codebasics/codebasics/dto/LearningPlanPhaseDTO.java
+++ codebasics/src/main/java/com/codebasics/codebasics/dto/LearningPlanPhaseDTO.java
@@ -1,2 +1,17 @@
-package com.codebasics.codebasics.dto;public class LearningPlanPhaseDTO {
+package com.codebasics.codebasics.dto;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LearningPlanPhaseDTO {
+    private Long id;
+    private Long learningPlanId;
+    private String topic;
+    private String skill;
+    private String description;
+    private String duration;
+    private String resources;
+    private String imageUrl;
 }
